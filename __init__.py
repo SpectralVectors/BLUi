4ae35418ab9c07d3f11803d0c584e17c48f01a93
SpectralVectors <robmuir1@gmail.com>
2021-10-05 07:46:46 -0400

    Splash Screen Support! And a backup keymap

--- __init__.py
+++ __init__.py
@@ -1,12 +1,18 @@
-# BLUI v0.8.1
+# BLUI v0.8.2
 
 import bpy
 from bpy.app.handlers import persistent
 
+@persistent
+def load_handler_for_startup(_):
+    print("Changing Startup Defaults!")
+
+    bpy.ops.wm.splash('INVOKE_DEFAULT')
 
 def register():
     print("Registering to Change Defaults")
     bpy.app.handlers.load_factory_startup_post.append(load_handler_for_startup)
+
 
 def unregister():
     print("Unregistering to Change Defaults")
